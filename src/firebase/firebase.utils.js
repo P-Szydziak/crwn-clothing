0df8c3afaff6d8a617caf3e8f3519ad6197e2fa7
Pawel Szydziak <szydziak.p@gmail.com>
2020-11-13 17:25:23 +0100

    implemented firebase utils, included to store authenticated users tnto firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,30 @@ const config = {
   appId: "1:884397889666:web:a88a4b0aa8f718b296fc9b",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user ", error.message);
+    }
+  }
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
