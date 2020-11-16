dac947c785b48389aafe0ee98b8de47feee50cec
Pawel Szydziak <szydziak.p@gmail.com>
2020-11-16 21:23:02 +0100

    move all app to styled components

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,7 +11,4 @@ if (process.env.NODE_ENV === "development") {
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
 export const persistor = persistStore(store);
-
-export default { store, persistStore };
